749419e09e46f95c1d8c506f607507c968dbd18e
Anthony Fabius <afabius05@gmail.com>
2023-09-24 20:24:46 -0400

    Files and laying out the rest of the forms

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ function App() {
   const formData = [
     { label: 'First Name', type: 'FirstName' },
     { label: 'Last Name', type: 'LastName' },
+    { label: 'Full Name', type: 'FullName' },
     { label: 'Email', type: 'Email'},
-    { label: 'Phone', type: 'Phone'},
+    { label: 'Mobile Phone Number', type: 'MobilePhone'},
+    { label: 'Work Phone Number', type: 'WorkPhone'}
   ];
 
   return (
@@ -20,12 +22,42 @@ function App() {
 
       <Form>
 
+        {/* Personal Information */}
         {formData.map((field, index) => (
           <Form.Group key={index} className="mb-3" controlId={`formGroup${field.type}`}>
             <Form.Label>{field.label}</Form.Label>
             <Form.Control type={field.type} placeholder={`Enter ${field.label}`} />
           </Form.Group>
         ))}
+
+        <hr></hr>
+        
+        {/* Files */}
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label> Resume </Form.Label>
+          <Form.Control type="file" />
+        </Form.Group>
+
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label> Cover Letter </Form.Label>
+          <Form.Control type="file" />
+        </Form.Group>
+
+
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label> Transcript </Form.Label>
+          <Form.Control type="file" />
+        </Form.Group>
+
+        {/* Education */}
+
+        
+
+        {/* Work Experience */}
+
+        
+
+        {/* Skills */}
 
       </Form>
     </Container>
